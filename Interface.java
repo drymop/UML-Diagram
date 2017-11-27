b7be4506926f5ffe8049df62667faaa9c163717a
lmtuan <lmtuan98@gmail.com>
2017-11-27 06:37:44 -0500

    Delete Interface.java

--- Interface.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashSet;
-
-public class Interface extends Structure {
-	private HashSet<Interface> superInterfaces;
-	private HashSet<Interface> subInterfaces;
-	
-	private HashSet<Method> methods;
-	
-}
